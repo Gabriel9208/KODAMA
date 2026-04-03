43b48a81289fe6e2f7b169e2257248ae724483d1
葉衍巖 <89693326+Gabriel9208@users.noreply.github.com>
2026-04-03 13:30:10 +0800

    Add: Dataset split script

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -1,16 +1,43 @@
 import yaml
+import json
+from sklearn.model_selection import train_test_split
 
 
-def split_dataset(config: dict) -> None:
-    with open('configs\split_config.yaml', 'r', encoding='utf-8') as f:
-        data = yaml.safe_load(f)
+def split_dataset(config: dict, seed: int) -> None:
+    with open('data/pipeline_progress.json') as f:
+        progress = json.load(f)
+
+    with open('configs/split_config.yaml') as f:
+        split_config = yaml.safe_load(f)
     
-    if data == None:
-        print("No config file found.")
+    if progress == None:
+        print("No data/pipeline_progress.json file found.")
         return
 
+    if split_config == None:
+        print("No configs/split_config.yaml file found.")
+        return
+
+    shards = [
+        v["shard_files"][0] 
+        for k, v in progress["sessions"].items() if v["status"] == "cleaned"
+    ]
+    
+    shards = list(set(shards))
+
+    train_shards, val_shards = train_test_split(
+        shards, 
+        test_size=split_config["split"]["val_portion"],
+        random_state=split_config["split"]["seed"]
+    )
+
+    split_config["split"]["train_shards"] = train_shards
+    split_config["split"]["val_shards"] = val_shards
+
+    with open('configs/split_config.yaml', 'w') as f:
+        yaml.dump(split_config, f)
 
     
 
 if __name__ == "__main__":
-    split_dataset(config)
+    split_dataset(config, 42)
