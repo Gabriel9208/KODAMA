80d4535f027b42c25bda1ec46b42e221d8de107d
Gabriel <yen08149203@gmail.com>
2026-04-06 15:21:48 +0800

    Fix: Split dataset by sessions(due to no overlapping across shards)

--- scripts/split_dataset.py
+++ scripts/split_dataset.py
@@ -2,6 +2,32 @@ import yaml
 import json
 from sklearn.model_selection import train_test_split
 
+def check_session() -> bool:
+    """
+        Check if sessions are assigned to the exactly one shard.
+    """
+    
+    with open('data/pipeline_progress.json') as f:
+        progress = json.load(f)
+
+    split_sessions = progress.get("sessions", {}).get("train", progress.get("sessions", {}))
+
+    overlap = False
+    for sid, v in split_sessions.items():
+        if "shard_files" not in v:
+            continue
+        if len(v["shard_files"]) > 1:
+            overlap = True
+            print(f"⚠️ Session {sid} spans multiple shards: {v['shard_files']}")
+
+    if overlap:
+        print("Overlap detected. Please run the pipeline again to fix the issue.")
+        return
+    
+    print("No overlap detected.")
+
+    return overlap
+    
 
 def split_dataset() -> None:
     with open('data/pipeline_progress.json') as f:
@@ -25,38 +51,14 @@ def split_dataset() -> None:
         for sid, v in split_sessions.items() if v["status"] == "cleaned"
     ]
 
-    for sid in sessions:
-        if len(split_sessions[sid]["shard_files"]) > 1:
-            print(f"⚠️ Session {sid} spans multiple shards: {split_sessions[sid]['shard_files']}")
-
     train_sessions, val_sessions = train_test_split(
         sessions,
         test_size=split_config["split"]["val_portion"],
         random_state=split_config["split"]["seed"]
     )
 
-    train_shards = set([
-        shard_files
-        for sid in train_sessions
-        for shard_files in split_sessions[sid]["shard_files"]
-    ])
-
-    val_shards = set([
-        shard_files
-        for sid in val_sessions
-        for shard_files in split_sessions[sid]["shard_files"]
-    ])
-
-    val_shards_clean = val_shards - train_shards
-    conflict_shards = val_shards - val_shards_clean
-    if conflict_shards:
-        print(f"{len(conflict_shards)} shards contain both train and val sessions.")
-        print("   These shards are assigned to train. Val samples inside are excluded.")
-        print(f"   Conflict shards: {conflict_shards}")
-        print(f"   Remain portion of validation shards: {len(val_shards_clean) / len(sessions):.3f}")
-
-    split_config["split"]["train_shards"] = sorted(list(train_shards))
-    split_config["split"]["val_shards"] = sorted(list(val_shards_clean))
+    split_config["split"]["train_sessions"] = sorted(list(train_sessions))
+    split_config["split"]["val_sessions"] = sorted(list(val_sessions))
 
     with open('configs/split_config.yaml', 'w') as f:
         yaml.dump(split_config, f)
@@ -64,4 +66,5 @@ def split_dataset() -> None:
     
 
 if __name__ == "__main__":
-    split_dataset()
+    if not check_session():
+        split_dataset()
