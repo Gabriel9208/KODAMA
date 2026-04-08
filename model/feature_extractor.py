af327820c52525af6037c9944d5ca514a8011328
Gabriel <yen08149203@gmail.com>
2026-04-08 17:31:55 +0800

    Fix: Feature extractor

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -1,49 +1,35 @@
-from ultralytics import YOLO
+import torch.nn as nn
 
-feature = {}
+class FeatureExtractor(nn.Module):
+    def __init__(self, model, layers:list[int]):
+        super().__init__()
+        self.model = model
+        self.layers = layers
+        self.features = {}
+        self.handles = []
 
-def hook_fn(name):
-    def hook(module, input, output):
-        feature[name] = output
-    return hook
+        for layer in self.layers:
+            self.handles.append(self.model.model.model[layer].register_forward_hook(self._hook_fn(layer)))
 
-model = YOLO("model/yolo26n-seg.pt")
-for i, m in enumerate(model.model.model):
-    print(f"Layer {i}: {type(m).__name__}")
+    def __del__(self):
+        for handle in self.handles:
+            handle.remove()
+        self.handles = [] 
 
-# Layer 0: Conv
-# Layer 1: Conv
-# Layer 2: C3k2
-# Layer 3: Conv
-# Layer 4: C3k2
-# Layer 5: Conv
-# Layer 6: C3k2
-# Layer 7: Conv
-# Layer 8: C3k2
-# Layer 9: SPPF
-# Layer 10: C2PSA
-# Layer 11: Upsample
-# Layer 12: Concat
-# Layer 13: C3k2
-# Layer 14: Upsample
-# Layer 15: Concat
-# Layer 16: C3k2
-# Layer 17: Conv
-# Layer 18: Concat
-# Layer 19: C3k2
-# Layer 20: Conv
-# Layer 21: Concat
-# Layer 22: C3k2
-# Layer 23: Segment26
+    def __enter__(self):
+        return self
 
-for idx in [16, 19, 22]:
-    model.model.model[idx].register_forward_hook(hook_fn(f"Layer{idx}"))
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__del__()
+            
+    def _hook_fn(self, name):
+        def hook(module, input, output):
+            self.features[name]  = output
+        return hook
 
-model.predict("https://ultralytics.com/images/bus.jpg", save=True)
+    def forward(self, X):
+        self.features = {}
 
-for f in feature:
-    print(f, feature[f].shape)
-
-# Layer16 torch.Size([1, 64, 80, 60]) -> P3
-# Layer19 torch.Size([1, 128, 40, 30]) -> P4
-# Layer22 torch.Size([1, 256, 20, 15]) -> P5
+        self.model(X)
+        return self.features
+        
