ab2cf1e3707927a0c88b0301069f44b86c6b42e1
Gabriel <yen08149203@gmail.com>
2026-04-14 18:41:31 +0800

    Add: sweep and readme

--- scripts/train_instance.py
+++ scripts/train_instance.py
@@ -1,52 +1,52 @@
+import os
+
 from ultralytics import YOLO
 import wandb
 
+
 wandb.login()
 
-model = YOLO("model/yolo26n-seg.pt")
+check_point = "runs/segment/KODAMA/yolo26n-seg_100_default_0/weights/last.pt"
+if os.path.exists(check_point):
+    model = YOLO(check_point)
+    model.train(resume=True)
+else:
+    model = YOLO("model/yolo26n-seg.pt")
 
-train_config = {
-    "epochs": 100,
-    "batch": 64,
-    "lr0": 1e-3,
-    "lrf": 1e-2,
-    "weight_decay": 0.0005
-}
+    train_config = {
+        "epochs": 100
+    }   
 
-data_aug = {   
-    "mixup": 0.1,         
-    "copy_paste": 0.2,    
-    "degrees": 10.0,   
-}
+    data_aug = {   
+        "mixup": 0.1,         
+        "copy_paste": 0.2,    
+        "degrees": 10.0,   
+    }   
 
-# name: 100_64 -> 100 epochs, batch size 64
-model.train(
-    name="yolo26n-seg_100_64",
-    project="KODAMA",
-    data="configs/sanpo.yaml",
+    # name: 100_64 -> 100 epochs, batch size 64
+    model.train(
+        name="yolo26n-seg_100_default_0",
+        project="KODAMA",
+        data="configs/sanpo.yaml",  
 
-    cos_lr=True,
+        cos_lr=True,    
 
-    lr0=train_config["lr0"],
-    lrf=train_config["lrf"],
-    weight_decay=train_config["weight_decay"],
-    epochs=train_config["epochs"],
-    batch=train_config["batch"],
+        epochs=train_config["epochs"],
 
-    imgsz=640,
-    workers=8,
-    patience=15,
-    seed=42,
-    device=[0, 1],
+        imgsz=640,
+        workers=8,
+        patience=15,
+        seed=42,
+        device=[0, 1],  
 
-    # data augmentation
-    mixup=data_aug["mixup"],
-    copy_paste=data_aug["copy_paste"],
-    degrees=data_aug["degrees"],
+        # data augmentation
+        mixup=data_aug["mixup"],
+        copy_paste=data_aug["copy_paste"],
+        degrees=data_aug["degrees"],    
 
-    exist_ok=True,
-    pretrained=True,
-    save=True,
-    verbose=True,
-    resume=True
-)
+        exist_ok=True,
+        pretrained=True,
+        save=True,
+        verbose=True,
+        resume=True
+    )
---
+++ scripts/tune_instance.py
@@ -0,0 +1,33 @@
+import os
+
+from ultralytics import YOLO
+
+model = YOLO("model/yolo26n-seg.pt")
+
+search_space = {
+    "lr0": (1e-5, 1e-2),
+    "lrf": (1e-4, 1e-2),
+    "weight_decay": (1e-3, 1e-2),
+    "momentum": (0.7, 0.98),
+    "batch": [32, 64, 128]
+}
+
+model.tune(
+    data="configs/sanpo.yaml", 
+    epochs=3, 
+    iterations=20, 
+    optimizer="AdamW", 
+    space=search_space,
+
+    device=[0, 1],
+    imgsz=640,
+    workers=8,
+
+    project="KODAMA",
+    name="yolo26n-seg_tune",
+    seed=42,
+    plots=False,
+    save=False,
+    val=False,
+    resume=True
+)
